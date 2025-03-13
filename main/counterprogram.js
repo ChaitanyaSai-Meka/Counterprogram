934ee65a091f89aa88cbe6bae0e3efc8e5c94979
Chaitanya Sai Meka <marvelchaitanya@gmail.com>
2025-03-14 03:15:29 +0530

    Add files via upload

---
+++ main/counterprogram.js
@@ -0,0 +1,17 @@
+const decrease=document.getElementById("decrease");
+const increase=document.getElementById("increase");
+const reset=document.getElementById("reset");
+const label=document.getElementById("number");
+let count=0;
+increase.onclick=function(){
+    count++;
+    number.textContent=count;
+}
+decrease.onclick=function(){
+    count--;
+    number.textContent=count;
+}
+reset.onclick=function(){
+    count=0;
+    number.textContent=count;
+}
